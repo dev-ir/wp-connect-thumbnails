bf337254ebad120a162b98ee0eea143fdf15ed62
unknown <info@developermen.ir>
2022-10-14 14:26:01 +0330

    Add button update thumbnail

--- inc/wexarama--admin-table.php
+++ inc/wexarama--admin-table.php
@@ -5,7 +5,8 @@ function dv_live_settings()
     <div class="container">
         <h4>تنظیمات سیستم</h4>
         <div>
-            <div class="button" id='get_update_counter'> Update Now </div>
+            <div class="button" id='get_update_counter'> Update Count Image's </div>
+            <div class="button" id='get_update_thumbnail'> Update Thumbnail </div>
         </div>
         <br>
         <div class="progress-bar">
@@ -16,9 +17,9 @@ function dv_live_settings()
             <thead>
                 <tr>
                     <th scope="col">ردیف</th>
+                    <th scope="col">شماره پست</th>
                     <th scope="col">نام پست</th>
                     <th scope="col">تعداد</th>
-                    <th scope="col">وضعیت</th>
                 </tr>
             </thead>
             <tbody id='the-list'></tbody>
