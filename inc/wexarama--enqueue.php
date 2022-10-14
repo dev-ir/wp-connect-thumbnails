3ad05dc9e8c11774cbc39173523ba56b51b6405b
unknown <info@developermen.ir>
2022-10-14 14:41:45 +0330

    Fix Bug extention file

--- inc/wexarama--enqueue.php
+++ inc/wexarama--enqueue.php
@@ -1,4 +1,4 @@
 <?php 
 add_action( 'admin_enqueue_scripts', function () {
-    wp_enqueue_style( 'wp-toranjapp-progress-bar', wexaram__dir_url.'assets/plugins/progress-bar/css/wexarama--progress-bar', array(), 1.1, 'all' );
+    wp_enqueue_style( 'wp-toranjapp-progress-bar', wexaram__dir_url.'assets/plugins/progress-bar/css/wexarama--progress-bar.css', array(), 1.1, 'all' );
 } );
