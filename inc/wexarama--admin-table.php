5f0a8c4cc1269a8e2c224543b00fded25f2bd23a
unknown <info@developermen.ir>
2022-10-14 13:01:59 +0330

    add admin table in submenu

---
+++ inc/wexarama--admin-table.php
@@ -0,0 +1,33 @@
+<?php
+function dv_live_settings()
+{ ?>
+
+    <div class="container">
+        <h4>تنظیمات سیستم</h4>
+        <div>
+            <div class="button" id='get_update_counter'> Update Now </div>
+        </div>
+        <br>
+        <table class="wp-list-table widefat fixed striped table-view-list posts">
+            <thead>
+                <tr>
+                    <th scope="col">ردیف</th>
+                    <th scope="col">نام پست</th>
+                    <th scope="col">تعداد</th>
+                    <th scope="col">وضعیت</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+
+            </tbody>
+        </table>
+    </div>
+
+
+<?php }
